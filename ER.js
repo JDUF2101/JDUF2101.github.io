ff1352536582f4d3ea03d5c45e079fdb4c1514f0
JDUF2101 <124031362+JDUF2101@users.noreply.github.com>
2023-02-06 17:49:31 -0600

    Add files via upload

---
+++ ER.js
@@ -0,0 +1,20 @@
+function funcionER(){
+    // document.getElementById() es una función que permite tomar elementos del documento (HTML) para 
+    // leer y modificar su contenido y/o propiedades. 
+    parrafo = document.getElementById("texto").innerHTML;
+    resultado = document.getElementById("salida");
+
+    /* La letra g al último de la expresión regular signigica "global" y es para que busque 
+    coincidencias con la expresión regular en todo el texto, si no se usa solo buscará en el 
+    primer renglón del texto. */
+    const expresion = /[A+E+I+O+Ua+e+i+o+u]+[a+e+i+o+u]/g;
+    /* console.log() es para imprimir en consola. Usar F12 y seleccionar la pestaña "Console" 
+    para ver la consola de JavaScript en Firefox y Chrome*/
+    palabras = parrafo.match(expresion);
+    console.log(palabras);
+    palabras.forEach(function (item) {
+        resultado.innerHTML = resultado.innerHTML + item + ", ";
+    });
+}
+// Ejecutar función
+funcionER();
